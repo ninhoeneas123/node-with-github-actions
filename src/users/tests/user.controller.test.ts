296d49fae5c701d89a2573edcc0b9cac871b763b
Seu ze <ninho.serafim95@gmail.co>
2023-05-18 16:38:02 -0300

    teste actions

--- src/users/tests/user.controller.test.ts
+++ src/users/tests/user.controller.test.ts
@@ -1,18 +1,23 @@
 import app from '../../app'
 import request from 'supertest'
 import {
-    describe, expect, it, jest, test
+    describe, expect, it, jest, test, afterAll, beforeAll
 } from '@jest/globals';
 
+
+
 let server: any
-beforeEach(() => {
+beforeAll(() => {
     const port = 3002;
     server = app.listen(port);
 });
 
-afterEach(() => {
-    server.close();
+
+afterAll((done) => {
+    console.log(done)
+    server.close(done);
 });
+
 describe('Test CREATE in users/create', () => {
     it('should return 201 when create author', async () => {
 
@@ -21,6 +26,6 @@ describe('Test CREATE in users/create', () => {
             .expect('Content-Type', /json/)
             .expect(200)
 
-            expect(response.body.message).toEqual("usuario criado com sucesso !")
+        expect(response.body.message).toEqual("usuario criado com sucesso !")
     })
 })
